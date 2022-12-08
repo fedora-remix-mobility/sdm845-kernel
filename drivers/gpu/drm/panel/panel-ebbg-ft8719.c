5b4e8e9e8f8e252b43e0d44ddb7742d110f5c55d
Caleb Connolly <caleb.connolly@linaro.org>
2022-12-08 19:52:20 +0000

    drm/panel: move some dsi commands from unprepare to disable
    The commit 007ac0262b0d ("drm/msm/dsi: switch to DRM_PANEL_BRIDGE")
    breaks panels which send DSI commands in their .unprepare callbacks.
    Migrate to using .disable for that for some SDM845 panels.
    
    Co-developed-by: Joel Selvaraj <joelselvaraj.oss@gmail.com>
    Signed-off-by: Joel Selvaraj <joelselvaraj.oss@gmail.com>
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

--- drivers/gpu/drm/panel/panel-ebbg-ft8719.c
+++ drivers/gpu/drm/panel/panel-ebbg-ft8719.c
@@ -72,8 +72,9 @@ static int ebbg_ft8719_on(struct ebbg_ft8719 *ctx)
 	return dsi_ctx.accum_err;
 }
 
-static int ebbg_ft8719_off(struct ebbg_ft8719 *ctx)
+static int ebbg_ft8719_disable(struct drm_panel *panel)
 {
+	struct ebbg_ft8719 *ctx = to_ebbg_ft8719(panel);
 	struct mipi_dsi_device *dsi = ctx->dsi;
 	struct mipi_dsi_multi_context dsi_ctx = { .dsi = dsi };
 
@@ -111,7 +112,6 @@ static int ebbg_ft8719_unprepare(struct drm_panel *panel)
 {
 	struct ebbg_ft8719 *ctx = to_ebbg_ft8719(panel);
 
-	ebbg_ft8719_off(ctx);
 	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
 	regulator_bulk_disable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
 
@@ -153,6 +153,7 @@ static int ebbg_ft8719_get_modes(struct drm_panel *panel,
 
 static const struct drm_panel_funcs ebbg_ft8719_panel_funcs = {
 	.prepare = ebbg_ft8719_prepare,
+	.disable = ebbg_ft8719_disable,
 	.unprepare = ebbg_ft8719_unprepare,
 	.get_modes = ebbg_ft8719_get_modes,
 };
@@ -198,6 +199,7 @@ static int ebbg_ft8719_probe(struct mipi_dsi_device *dsi)
 
 	drm_panel_init(&ctx->panel, dev, &ebbg_ft8719_panel_funcs,
 		       DRM_MODE_CONNECTOR_DSI);
+	ctx->panel.prepare_prev_first = true;
 
 	ret = drm_panel_of_backlight(&ctx->panel);
 	if (ret)
